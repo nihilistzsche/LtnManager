36b91b8ae9e0cdcbcf22d7be19b09e36f2800fe9
raiguard <caleb.heuer@gmail.com>
2020-04-29 17:00:08 -0600

    Syntax fixes

--- src/data.lua
+++ src/data.lua
@@ -1,6 +1,3 @@
--- -------------------------------------------------------------------------------------------------------------------------------------------------------------
--- PROTOTYPES
-
 local util = require("prototypes.util")
 
 data:extend{
@@ -31,21 +28,5 @@ data:extend{
   }
 }
 
--- the rest
 require("prototypes.sprite")
 require("prototypes.style")
-
---[[
-  local tile = table.deepcopy(data.raw["tile"]["lab-dark-2"])
-  tile.name = "ltnm-frame-tile"
-  tile.map_color = {64, 63, 64}
-  data:extend{tile}
-
-  local rail = table.deepcopy(data.raw["straight-rail"]["straight-rail"])
-  rail.name = "ltnm-frame-rail"
-  rail.order = "z"
-  rail.map_color = {r=64, g=63, b=64}
-  rail.friendly_map_color = {r=64, g=63, b=64}
-  rail.enemy_map_color = {r=64, g=63, b=64}
-  data:extend{rail}
-]]
