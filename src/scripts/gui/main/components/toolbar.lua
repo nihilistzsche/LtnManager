8a19ab40c8a97b71a0494454e6e5d653cc22232a
raiguard <caleb.heuer@gmail.com>
2020-09-30 22:44:47 -0600

    Better mock toolbar

--- src/scripts/gui/main/components/toolbar.lua
+++ src/scripts/gui/main/components/toolbar.lua
@@ -3,10 +3,12 @@ local component = require("lib.gui-component")()
 function component.build()
   return (
     {type = "frame", style = "subheader_frame", bottom_margin = 12, children = {
+      {type = "label", style = "subheader_caption_label", right_margin = 8, caption = "Search:"},
       {type = "textfield", lose_focus_on_confirm = true, clear_and_focus_on_right_click = true, text = ""},
-      -- {type = "label", caption = "TODO: search funcs"},
       {type = "empty-widget", style = "flib_horizontal_pusher"},
-      {type = "label", style = "subheader_caption_label", caption = "Surface:"},
+      {type = "label", style = "subheader_caption_label", right_margin = 8, caption = "Network ID:"},
+      {type = "textfield", width = 50, caption = "-1"},
+      {type = "label", style = "subheader_caption_label", right_margin = 8, caption = "Surface:"},
       {type = "drop-down", items = {"(all)", "nauvis"}, selected_index = 2}
     }}
   )
