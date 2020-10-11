5ff1f391170fc70818dcb7ab242f6b233ff11afe
raiguard <caleb.heuer@gmail.com>
2020-10-11 12:07:04 -0600

    Use new flib indicator sprites

--- src/scripts/gui/main/components/common/status-indicator.lua
+++ src/scripts/gui/main/components/common/status-indicator.lua
@@ -1,7 +1,8 @@
 return function(name, count, dark, width)
+  local color = string.gsub(name, "signal%-", "")
   return (
     {type = "flow", horizontal_align = "center", vertical_align = "center", width = width, children = {
-      {type = "sprite", style = "status_image", sprite = "ltnm_status_"..name},
+      {type = "sprite", style = "flib_indicator", sprite = "flib_indicator_"..color},
       {type = "label", style = dark and "ltnm_black_label" or "label", caption = count}
     }}
   )
