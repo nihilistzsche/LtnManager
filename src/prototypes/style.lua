f7110061b30eb0d8b656642c76eab7d1e36db953
raiguard <caleb.heuer@gmail.com>
2021-11-13 00:48:53 -0700

    Remove dead code

--- src/prototypes/style.lua
+++ src/prototypes/style.lua
@@ -22,25 +22,6 @@ for _, color in ipairs({ "default", "red", "green", "blue" }) do
   }
 end
 
--- styles.ltnm_depot_button = {
---   type = "button_style",
---   parent = "button",
---   width = 206,
---   height = 89,
---   padding = 4,
---   top_padding = 1,
---   hovered_font_color = button_hovered_font_color,
---   hovered_graphical_set = {
---     base = {position = {34, 17}, corner_size = 8},
---     shadow = default_dirt,
---   },
---   disabled_font_color = button_hovered_font_color,
---   disabled_graphical_set = {
---     base = {position = {225, 17}, corner_size = 8},
---     shadow = default_dirt
---   }
--- }
-
 styles.ltnm_train_minimap_button = {
   type = "button_style",
   parent = "button",
@@ -123,25 +104,7 @@ styles.ltnm_selected_sort_checkbox = {
   },
 }
 
--- EMPTY WIDGET STYLES
-
--- styles.ltnm_table_header_spacer = {
---   type = "empty_widget_style",
---   width = 12
--- }
-
 -- FLOW STYLES
-
--- styles.ltnm_depot_button_inner_flow = {
---   type = "vertical_flow_style",
---   vertical_spacing = 0
--- }
-
--- styles.ltnm_train_status_flow = {
---   type = "vertical_flow_style",
---   vertical_spacing = 0,
---   vertical_align = "center",
--- }
 
 styles.ltnm_warning_flow = {
   type = "horizontal_flow_style",
@@ -288,27 +251,10 @@ styles.ltnm_main_warning_frame = {
 
 -- LABEL STYLES
 
--- styles.ltnm_black_label = {
---   type = "label_style",
---   parent = "label",
---   font_color = button_default_font_color
--- }
-
--- styles.ltnm_bold_black_label = {
---   type = "label_style",
---   parent = "bold_label",
---   font_color = button_default_bold_font_color
--- }
-
 local hovered_label_color = {
   r = 0.5 * (1 + default_orange_color.r),
   g = 0.5 * (1 + default_orange_color.g),
   b = 0.5 * (1 + default_orange_color.b),
-}
-
-styles.ltnm_semibold_label = {
-  type = "label_style",
-  font = "default-semibold",
 }
 
 styles.ltnm_clickable_semibold_label = {
@@ -318,31 +264,6 @@ styles.ltnm_clickable_semibold_label = {
   disabled_font_color = hovered_label_color,
 }
 
-styles.ltnm_clickable_label = {
-  type = "label_style",
-  parent = "ltnm_clickable_semibold_label",
-  font = "default",
-}
-
-styles.ltnm_bold_green_label = {
-  type = "label_style",
-  parent = "bold_label",
-  font_color = constants.colors.green.tbl,
-}
-
-styles.ltnm_bold_red_label = {
-  type = "label_style",
-  parent = "bold_label",
-  font_color = constants.colors.red.tbl,
-}
-
--- styles.ltnm_semibold_black_label = {
---   type = "label_style",
---   parent = "bold_label",
---   font = "default-semibold",
---   font_color = button_default_font_color
--- }
-
 styles.ltnm_minimap_label = {
   type = "label_style",
   font = "default-game",
@@ -351,6 +272,11 @@ styles.ltnm_minimap_label = {
   vertical_align = "bottom",
   horizontal_align = "right",
   right_padding = 4,
+}
+
+styles.ltnm_semibold_label = {
+  type = "label_style",
+  font = "default-semibold",
 }
 
 -- MINIMAP STYLES
@@ -371,25 +297,6 @@ styles.ltnm_table_scroll_pane = {
   },
 }
 
--- styles.ltnm_depot_select_scroll_pane = {
---   type = "scroll_pane_style",
---   parent = "flib_naked_scroll_pane_no_padding",
---   width = 206,
---   height = (depot_button_height * 8),
---   background_graphical_set = {
---     position = {282, 17},
---     corner_size = 8,
---     overall_tiling_horizontal_padding = 6,
---     overall_tiling_vertical_padding = 6,
---     overall_tiling_vertical_size = depot_button_height - 12,
---     overall_tiling_vertical_spacing = 12
---   },
---   vertical_flow_style = {
---     type = "vertical_flow_style",
---     vertical_spacing = 0
---   }
--- }
-
 styles.ltnm_slot_table_scroll_pane = {
   type = "scroll_pane_style",
   parent = "flib_naked_scroll_pane_no_padding",
@@ -407,51 +314,6 @@ styles.ltnm_slot_table_scroll_pane = {
     },
   },
 }
-
--- styles.ltnm_small_slot_table_scroll_pane = {
---   type = "scroll_pane_style",
---   parent = "flib_naked_scroll_pane",
---   padding = 0,
---   margin = 0,
---   extra_padding_when_activated = 0,
---   minimal_height = 36,
---   dont_force_clipping_rect_for_contents = false,
---   background_graphical_set = {
---     base = {
---       position = {282, 17},
---       corner_size = 8,
---       overall_tiling_horizontal_padding = 4,
---       overall_tiling_horizontal_size = 28,
---       overall_tiling_horizontal_spacing = 8,
---       overall_tiling_vertical_padding = 4,
---       overall_tiling_vertical_size = 28,
---       overall_tiling_vertical_spacing = 8
---     }
---   }
--- }
-
--- styles.ltnm_table_scroll_pane = {
---   type = "scroll_pane_style",
---   extra_padding_when_activated = 0,
---   padding = 0,
---   horizontally_stretchable = "on",
---   vertically_stretchable = "on",
---   graphical_set = {
---     shadow = default_inner_shadow
---   },
---   background_graphical_set = {
---     position = {282, 17},
---     corner_size = 8,
---     overall_tiling_horizontal_padding = 6,
---     overall_tiling_vertical_padding = 6,
---     overall_tiling_vertical_size = 40,
---     overall_tiling_vertical_spacing = 12
---   },
---   vertical_flow_style = {
---     type = "vertical_flow_style",
---     vertical_spacing = 0
---   }
--- }
 
 -- TABBED PANE STYLES
 
